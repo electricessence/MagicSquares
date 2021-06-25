b451e413d98208f35d36fc7a54372bedb637c363
electricessence <5899455+electricessence@users.noreply.github.com>
2021-06-25 16:35:15 -0700

    Added 4 square test.

--- MagicSquaresTests/MagicSquareTests.cs
+++ MagicSquaresTests/MagicSquareTests.cs
@@ -15,30 +15,45 @@ namespace MagicSquaresTests
         public void IsMagicSquareTest()
         {
             {
-                int[][] square = new int[3][] {
-                    new int[3] { 2, 4, 9 },
-                    new int[3] { 6, 8, 1 },
-                    new int[3] { 7, 3, 5 }
+                int[][] square = new int[][] {
+                    new int[] { 2, 4, 9 },
+                    new int[] { 6, 8, 1 },
+                    new int[] { 7, 3, 5 }
                 };
 
                 Assert.IsTrue(square.IsMagicSquare());
             }
 
             {
-                int[][] square = new int[3][] {
-                    new int[3] { 2, 4, 9 },
-                    new int[3] { 6, 8, 1 },
-                    new int[3] { 7, 3, 6 }
+                int[][] square = new int[][] {
+                    new int[] { 4, 14, 15, 1 },
+                    new int[] { 9, 7, 6, 12 },
+                    new int[] { 5, 11, 10, 8 },
+                    new int[] { 16, 2, 3, 13 }
+                };
+
+                Assert.IsTrue(square.IsMagicSquare());
+            }
+        }
+
+        [TestMethod]
+        public void IsNotMagicSquareTest()
+        {
+            {
+                int[][] square = new int[][] {
+                    new int[] { 2, 4, 9 },
+                    new int[] { 6, 8, 1 },
+                    new int[] { 7, 3, 6 }
                 };
 
                 Assert.IsFalse(square.IsMagicSquare());
             }
 
             {
-                int[][] square = new int[3][] {
-                    new int[3] { 2, 4, 9 },
-                    new int[4] { 6, 7, 1, 1 },
-                    new int[3] { 7, 4, 5 }
+                int[][] square = new int[][] {
+                    new int[] { 2, 4, 9 },
+                    new int[] { 6, 7, 1, 1 },
+                    new int[] { 7, 4, 5 }
                 };
 
                 Assert.IsFalse(square.IsMagicSquare());
