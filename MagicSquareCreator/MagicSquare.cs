cecc13bc02bbf6f5c4ad299c96a42090ef3c9cda
electricessence <5899455+electricessence@users.noreply.github.com>
2021-06-26 14:54:36 -0700

    Added quick creation that works with odd sizes.

---
+++ MagicSquareCreator/MagicSquare.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSquareCreator
+{
+	public class MagicSquare
+	{
+		public static int[,] CreateFromFirst(int size, int first)
+		{
+			var bounds = first + size * size;
+			var grid = new int[size, size];
+			var x = size / 2;
+			var y = size - 1;
+			//if (size % 2 == 0) --x;
+
+			void Escape()
+			{
+				x -= 1;
+				y -= 2;
+			}
+
+			void EnsureTargetCell()
+			{
+				while (true)
+				{
+					if (x == size && y == size) Escape();
+					else if (x == size) x = 0;
+					else if (y == size) y = 0;
+					if (grid[x, y] == 0) break;
+					else Escape();
+					if (x < 0) x = size + x;
+					if (y < 0) y = size + y;
+				}
+			}
+
+			for (var i = first; i < bounds; i++)
+			{
+				EnsureTargetCell();
+				grid[x, y] = i;
+				++x;
+				++y;
+			}
+
+			return grid;
+		}
+	}
+}
---
+++ MagicSquareCreator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MagicSquareCreator
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.Write("Enter the size of the desired Magic Square: ");
+			var size = int.Parse(Console.ReadLine());
+
+			Console.Write("Enter the starting number: ");
+			var first = int.Parse(Console.ReadLine());
+			Console.WriteLine();
+
+			var grid = MagicSquare.CreateFromFirst(size, first);
+
+			var colSum = new int[size];
+			for (var y = size - 1; y >= 0; --y)
+			{ 
+				var rowSum = 0;
+				for(var x = 0; x<size; ++x)
+				{
+					var v = grid[x, y];
+					rowSum += v;
+					colSum[x] += v;
+					Console.Write(v);
+					Console.Write(' ');
+				}
+				Console.Write(" = ");
+				Console.WriteLine(rowSum);
+			}
+			Console.WriteLine("_____________");
+			Console.WriteLine(string.Join(' ', colSum));
+		}
+
+
+	}
+}
